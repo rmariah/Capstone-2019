75b322c0fc1c9b77105ff6dfce1099e4ac166134
Kylebunzel <ksbunzel@uwm.edu>
2019-11-14 23:42:43 -0600

    Installed request package

--- ScrappingUI/minime/views.py
+++ ScrappingUI/minime/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 
+import requests
+
+
 class HomePageView(View):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
@@ -8,5 +11,7 @@ class HomePageView(View):
 
 class Elastic(View):
     def get(self, request):
+        # result = requests.get(your_link)
+        print(result)
         return render(request, "elastic.html", {})
 
