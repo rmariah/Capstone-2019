22b04726106ddfc85e5a9c4da510bdc96f4363b2
Kylebunzel <ksbunzel@uwm.edu>
2019-11-19 23:22:09 -0600

    Fixed ElasticSearch

--- ScrappingUI/minime/views.py
+++ ScrappingUI/minime/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 import requests
-import json
+import elasticsearch
 
 
 def cleanSearch(data):
@@ -29,11 +29,9 @@ def cleanSearch(data):
 
 
 def search(term):
-    base_url = "http://167.71.250.175:1371/filebeat-7.4.2-2019.11.13-000001/_search?q=text: * "
-    search_term = term
-    req_url = base_url + search_term + " *"
-    data = requests.get(req_url).json()["hits"]["hits"]
-    print(req_url)
+    es = elasticsearch.Elasticsearch([{'host': '167.71.250.175', 'port': 1371}])
+    search_obj = {'query': {'wildcard': {'text': '* ' + term + ' *'}}}
+    data = es.search(index="filebeat-7.4.2-2019.11.13-000001", body=search_obj)["hits"]["hits"]
     return cleanSearch(data)
 
 class HomePageView(View):
