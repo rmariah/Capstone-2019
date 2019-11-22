e0c897883b4434b05bbb43cdb106a6577d877e63
Kylebunzel <ksbunzel@uwm.edu>
2019-11-21 23:28:18 -0600

    Set up static branch

--- ScrappingUI/minime/views.py
+++ ScrappingUI/minime/views.py
@@ -29,20 +29,11 @@ def cleanSearch(data):
 
 
 def search(term):
-<<<<<<< HEAD
-    base_url = "http://167.71.250.175:1371/filebeat-7.4.2-2019.11.13-000001/_search?q=text: * "
-    search_term = term
-    req_url = base_url + search_term + " *"
-    data = requests.get(req_url).json()["hits"]["hits"]
-    print(req_url)
-    
-    
-=======
     es = elasticsearch.Elasticsearch([{'host': '167.71.250.175', 'port': 1371}])
     search_obj = {'query': {'wildcard': {'text': '* ' + term + ' *'}}}
     data = es.search(index="filebeat-7.4.2-2019.11.13-000001", body=search_obj)["hits"]["hits"]
->>>>>>> 22b04726106ddfc85e5a9c4da510bdc96f4363b2
     return cleanSearch(data)
+
 
 class HomePageView(View):
     def get(self, request, **kwargs):
@@ -50,14 +41,17 @@ class HomePageView(View):
 #         data = requests.get('elastic.html').json()
         # ignore above for now.
         return render(request, 'index.html', {"data": data})
+
     def post(self, request):
         data = search(request.POST["search"])
         return render(request, "index.html", {"data": data})
 
+
 class UserAccount(View):
-	def get(self, request, **kwargs):
-		data = None
-		return render(request, 'useraccount.html', {"data":data})
+    def get(self, request, **kwargs):
+        data = None
+        return render(request, 'useraccount.html', {"data":data})
+
 
 class Elastic(View):
     def get(self, request):
