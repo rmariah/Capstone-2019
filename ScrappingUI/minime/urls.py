48a3a9a98b90bc03dd7fa60522cd0e36fe1e9f11
Mariah Ryan <ryan34@uwm.edu>
2019-12-04 13:50:54 -0600

    Committing...

--- ScrappingUI/minime/urls.py
+++ ScrappingUI/minime/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.conf.urls import url
 from django.views.generic import RedirectView
+from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'home/', views.HomePageView.as_view()),
     url('elastic/', views.Elastic.as_view()),
     url(r'useraccount/', views.UserAccount.as_view()),
-    url(r'accounts/logout/', RedirectView.as_view(url='accounts/logout/')),
+    url('accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/logout/', RedirectView.as_view(url='accounts/login/')),
 ]
