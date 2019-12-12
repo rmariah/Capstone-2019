aab1313de4d69408473e634c9332996aed0c04a4
Kylebunzel <ksbunzel@uwm.edu>
2019-12-11 21:38:04 -0600

    Added News

--- ScrappingUI/ScrappingUI/views.py
+++ ScrappingUI/ScrappingUI/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 import random
-
+import pandas as pd
 
 def cleanSearch(data):
     array = []
@@ -32,29 +32,61 @@ def cleanSearch(data):
         array.append(newArray)
     return array
 
+def getSite(link):
+    sites = {'abc':'https://vignette.wikia.nocookie.net/logopedia/images/d/d7/Abc_2013_logo_dark_grey.svg/revision/latest/scale-to-width-down/200?cb=20180927202336',
+             'cnn':'https://vignette.wikia.nocookie.net/logopedia/images/5/52/CNN_%282014%29.svg/revision/latest/scale-to-width-down/200?cb=20180509053533',
+             'bbc':'https://vignette.wikia.nocookie.net/logopedia/images/e/eb/BBC.svg/revision/latest/scale-to-width-down/250?cb=20170913090831',
+             'fox':'https://vignette.wikia.nocookie.net/logopedia/images/e/e9/FoxBCLogo2019.svg/revision/latest/scale-to-width-down/200?cb=20191121082502',
+             'nyt':'https://vignette.wikia.nocookie.net/logopedia/images/d/d7/NYT_Masthead.svg/revision/latest/scale-to-width-down/250?cb=20141210032555',
+             'washingtonpost':'https://vignette.wikia.nocookie.net/logopedia/images/c/c4/Washington-post-logo-thumb.jpg/revision/latest/scale-to-width-down/150?cb=20140719115207'}
 
-def search(term):
+    for site in sites.keys():
+        if (link.count(site) > 0):
+            return sites[site]
+
+def cleanSearchNews(data):
+    array = []
+    for x in data:
+        x = x.get('_source')
+        newArray = {
+            "summary": x.get('summary'),
+            "title": x.get('title'),
+            "link": x.get('link'),
+            "logo": getSite(x.get('link')),
+            "date": (pd.to_datetime(x.get('published'))).strftime("%b %d, %Y at %I:%M %p"),
+        }
+        array.append(newArray)
+    return array
+
+
+def searchTweets(term):
     es = elasticsearch.Elasticsearch([{'host': '167.71.250.175', 'port': 1371}])
-    search_obj = {'query': {'wildcard': {'text': '* ' + term + ' *'}}}
-    data = es.search(index="filebeat-7.4.2-2019.11.13-000001", body=search_obj)["hits"]["hits"]
+    search_obj = {'query': {'bool': {'must': [{'match': {'tags': 'twitter'}}, {'wildcard': {'text': '* ' + term + ' *'}}]}}}
+    data = es.search(index="filebeat-7.4.2-2019.12.10-000001", body=search_obj)["hits"]["hits"]
     return cleanSearch(data)
 
 
-def chooseRandom():
-    topics = ["school", "uwm", "university of wisconsin-milwaukee", "capstone", "university", "wisconsin", "milwaukee"]
-    return search(random.choice(topics))
+def searchNews(term):
+    es = elasticsearch.Elasticsearch([{'host': '167.71.250.175', 'port': 1371}])
+    search_obj = {'query': {'bool': {'must': [{'match': {'tags': 'news'}}, {'wildcard': {'title': '* ' + term + ' *'}}]}}}
+    data = es.search(index="filebeat-7.4.2-2019.12.10-000001", body=search_obj)["hits"]["hits"]
+    return cleanSearchNews(data)
 
+
+def chooseRandom():
+    topics = ["school", "test"]
+    return random.choice(topics)
 
 class HomePageView(View):
     def get(self, request, **kwargs):
-        data = None
-#         data = requests.get('elastic.html').json()
-        # ignore above for now.
-        return render(request, 'index.html', {"data": chooseRandom()})
+        topic = chooseRandom()
+        news, tweets = searchNews(topic), searchTweets(topic)
+        return render(request, 'index.html', {"tweets": tweets, "news": news})
 
     def post(self, request):
-        data = search(request.POST["search"])
-        return render(request, "index.html", {"data": data})
+        news, tweets = searchNews(request.POST["search"]), searchTweets(request.POST["search"])
+        return render(request, 'index.html', {"tweets": tweets, "news": news})
+
 
 
 class UserAccount(View):
@@ -62,17 +94,15 @@ class UserAccount(View):
         data = None
         return render(request, 'useraccount.html', {"data":data})
 
-
 class Main(View):
     def get(self, request, **kwargs):
-        data = None
-#         data = requests.get('elastic.html').json()
-        # ignore above for now.
-        return render(request, 'registration/main.html', {"data": chooseRandom()})
+        topic = chooseRandom()
+        news, tweets = searchNews(topic), searchTweets(topic)
+        return render(request, 'index.html', {"tweets": tweets, "news": news})
 
     def post(self, request):
-        data = search(request.POST["search"])
-        return render(request, "regristration/main.html", {"data": data})
+        news, tweets = searchNews(request.POST["search"]), searchTweets(request.POST["search"])
+        return render(request, 'index.html', {"tweets": tweets, "news": news})
 
 
 def signup(request):
@@ -101,5 +131,4 @@ class Elastic(View):
         return render(request, "elastic.html")
 
     def post(self, request):
-        data = search(request.POST["search"])
-        return render(request, "elastic.html", {"data": data})
+        return render(request, "elastic.html", {"data": None})
